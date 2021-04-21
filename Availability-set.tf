6c34d626ea11e2727b472b78d4b9884abfe04c04
Harsha114 <harshapilli3007@gmail.com>
2021-04-21 10:55:15 +0000

    Create availability set

---
+++ Availability-set.tf
@@ -0,0 +1,18 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_availability_set" "example" {
+  name                = "example-aset"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  tags = {
+    environment = "Production"
+  }
+}
